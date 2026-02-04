e89a6cd96469ac69e9d66b13523bfc7f9b74c404
jburnstu <75335189+jburnstu@users.noreply.github.com>
2026-02-04 14:12:23 +0000

    Preliminary notes added

--- chainlettersstories/tests.py
+++ chainlettersstories/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+'''
+Checks on status:
+- if a 1,2 or 3, assert previous are 5, and assert not previousid of anything
+- if in moderationassignment and not closed, assert 3
+- if no 1, 2, or 3 following, assert not 5
+
+'''
+
+'''
+Checks on login details:
+- assert an existing username will prompt login
+- assert an existing login will login
+- assert an empty username / password will prompt a message
+
+'''
