f019efb0b71eb99f0a0001a9b3cd572af6009e78
jburnstu <75335189+jburnstu@users.noreply.github.com>
2026-02-04 13:47:43 +0000

    Buttons are set up I think! need to be checked on both tabs (but that might be easier once we reset the story bank)

--- chainlettersstories/static/chainlettersstories/write_dashboard_script.js
+++ chainlettersstories/static/chainlettersstories/write_dashboard_script.js
@@ -1,9 +1,9 @@
 // const story_list = JSON.parse(document.getElementById('story-data').textContent);
 
-storiesHeaderPanel = document.querySelector(".stories-header-panel");
-storiesMainPanel = document.querySelector(".stories-main-panel");
-joinNewStoryButton = document.querySelector(".join-button");
-
+const storiesHeaderPanel = document.querySelector(".stories-header-panel");
+const storiesMainPanel = document.querySelector(".stories-main-panel");
+const joinNewStoryButton = document.querySelector(".join-button");
+const tabButtons = document.querySelectorAll("tab-button");
 
 var stories;
 var story_excluding_last_section;
@@ -13,58 +13,63 @@ var storyTabHTML;
 // var storiesHeaderPanel;
 // var storiesMainPanel;
 
-
-const createStoryTabsAndButtons = function (stories) {
-    storiesHeaderPanel.innerHTML = '';
-    storiesMainPanel.innerHTML = '';
-
-    console.log(stories);
-    console.log(typeof (stories));
-
-    stories.forEach(function (story, i) {
-
-        story_excluding_last_section = story[1].slice(0, -1).join(" ");
-        last_section = story[1].slice(-1).join(" ");
-
-        console.log(last_section);
-        console.log(typeof (last_section))
-
-
-        storyButtonHTML = `
-        <button class="story-tab-button" id="story-tab-button-${i}">${i}</button>
-        `;
-
-        storyTabHTML = `
-        <div class="container-story-tab" id = "container-story-tab-${i}">
-            <div class="read-only-div" id="read-only-div-${i}">${story_excluding_last_section}.</div> 
-            <form action= "????" method="post"
-                <fieldset>
-                    <input class=section-input id="section-input-${i}" 
-                    type="text" placeholder="Enter Story Here" name="content" 
-                    value= "${last_section}"> 
-                </fieldset>
-                <input type="submit" value="Submit">
-            </form>
-        </div>
-        `;
-
-
-        storiesHeaderPanel.insertAdjacentHTML('afterbegin', storyButtonHTML);
-        storiesMainPanel.insertAdjacentHTML('afterbegin', storyTabHTML);
-    });
-
+$(".story-tab-button").click(function () {
+    $(".container-story-tab").hide();
+    id = $(this).attr("id").slice(-1);
+    thisPanelId = "#container-story-tab-" + id
+    console.log(thisPanelId)
+    console.log()
+    $(thisPanelId).show();
 }
+)
+// if (story_list) {
+//     console.log("story_list found");
 
 
-
-
-if (story_list) {
-    console.log("story_list found");
-    // createStoryTabsAndButtons(story_list);
-    // $(".read-only-div").click(function () {
-    //     $.post()
-    // })
-
-}
+// }
 console.log("page loaded");
+// }
+
+
+
+
+// const createStoryTabsAndButtons = function (stories) {
+//     storiesHeaderPanel.innerHTML = '';
+//     storiesMainPanel.innerHTML = '';
+
+//     console.log(stories);
+//     console.log(typeof (stories));
+
+//     stories.forEach(function (story, i) {
+
+//         story_excluding_last_section = story[1].slice(0, -1).join(" ");
+//         last_section = story[1].slice(-1).join(" ");
+
+//         console.log(last_section);
+//         console.log(typeof (last_section))
+
+
+//         storyButtonHTML = `
+//         <button class="story-tab-button" id="story-tab-button-${i}">${i}</button>
+//         `;
+
+//         storyTabHTML = `
+//         <div class="container-story-tab" id = "container-story-tab-${i}">
+//             <div class="read-only-div" id="read-only-div-${i}">${story_excluding_last_section}.</div>
+//             <form action= "????" method="post"
+//                 <fieldset>
+//                     <input class=section-input id="section-input-${i}"
+//                     type="text" placeholder="Enter Story Here" name="content"
+//                     value= "${last_section}">
+//                 </fieldset>
+//                 <input type="submit" value="Submit">
+//             </form>
+//         </div>
+//         `;
+
+
+//         storiesHeaderPanel.insertAdjacentHTML('afterbegin', storyButtonHTML);
+//         storiesMainPanel.insertAdjacentHTML('afterbegin', storyTabHTML);
+//     });
+
 // }
