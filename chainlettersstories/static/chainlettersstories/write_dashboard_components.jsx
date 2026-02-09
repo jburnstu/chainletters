c17ff0c1a5cee0f0e49232d2b366dd95b910f4cf
jburnstu <75335189+jburnstu@users.noreply.github.com>
2026-02-09 16:56:47 +0000

    We're doing a lot of writing here... realistically, I think at some point we need to hop over to a completely fresh react-based app.

--- chainlettersstories/static/chainlettersstories/write_dashboard_components.jsx
+++ chainlettersstories/static/chainlettersstories/write_dashboard_components.jsx
@@ -1,8 +1,6 @@
 
-
-
-// import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
 
 
@@ -21,6 +19,173 @@ function StorySoFarSplitOut() {
 
 }
 
+function LinkButton(props) {
+    return (
+        <form action="{props.link} {props.userid}">
+            <button type="submit">{props.name}</button>
+        </form>
+    )
+}
+
+function GetButton(props) {
+    return (
+        <form action="{props.link} {props.userid}">
+            <button type="submit">{props.name}</button>
+        </form>
+    )
+}
+
+function UniversalHeader(props) {
+    return (
+        <header>
+            <h1>CHAIN MATES</h1>
+            <h1>Hi, {props.displayname}!</h1>
+            <LinkButton userid={props.userid} link='chainlettersstories:dashboard' name="DASHBOARD" />
+            <LinkButton userid={props.userid} link='chainlettersstories:write_dashboard' name="WRITE" />
+            <LinkButton userid={props.userid} link='chainlettersstories:read_dashboard' name="READ" />
+            <LinkButton userid={props.userid} link='chainlettersstories:login_or_signup_page' name="LOG OUT" />
+        </header >
+    )
+}
+
+function WriteSidebar() {
+    return (
+        <div class="sidebar">
+            <GetButton userid={props.userid} link='chainlettersstories:create_new_story' name="NEW" />
+            <GetButton userid={props.userid} link='chainlettersstories:get_random_available_section' name="JOIN" />
+        </div>
+    )
+}
+
+function Tabs(props) {
+
+    return (
+        <>
+            {props.stories.map(story =>
+                <button class="story-tab-button"
+                    id="story-tab-button-{{forloop.counter}}">{{}}</button>
+            )}
+        </>
+    )
+}
+
+function StoryContent(props) {
+
+}
+
+function WriteDashboard(props) {
+
+    writeDashbaordHTML = (<>
+        <UniversalHeader userid={props.userid} displayname={props.displayname} />
+        <WriteSidebar />
+        <Tabs />
+        <StoryContent />
+        <SubmitButtons />
+        <Comments />
+    </>
+    )
+
+}
+
+function ReadDashboard(props) { }
+
+function Dashboard(props) { }
+
+function AppByUser(props) {
+    return (
+        <BrowserRouter>
+            {/* Navigation */}
+            <nav>
+
+                <Link to="/">Dashboard</Link> |{" "}
+                <Link to="/products">Products</Link> |{" "}
+                <Link to="/contact">Contact</Link>
+            </nav>
+
+            {/* Routes */}
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/products" element={<Write />}>
+                    <Route path="car" element={<CarProducts />} />
+                    <Route path="bike" element={<BikeProducts />} />
+                </Route>
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+
+
 createRoot(document.getElementById('react-read-only-content')).render(
     <StorySoFarSplitOut />
 );
+
+
+
+<div class="container">
+    <header>
+        <h1>CHAIN MATES</h1>
+        <h1>Hi, {{ displayname }}!</h1>
+        <form action="{% url 'chainlettersstories:dashboard' userid %}">
+            <button type="submit">DASHBOARD</button>
+        </form>
+        <form action="{% url 'chainlettersstories:write_dashboard' userid %}">
+            <button type="submit">WRITE</button>
+        </form>
+        <form action="{% url 'chainlettersstories:read_dashboard' userid %}">
+            <button type="submit">READ</button>
+        </form>
+        <form action="{% url 'chainlettersstories:login_or_signup_page'%}">
+            <button type="submit">LOG OUT</button>
+        </form>
+    </header>
+
+    <div class="sidebar">
+        <form action="{% url 'chainlettersstories:create_new_story' userid %}">
+            <button type="submit">New</button>
+        </form>
+        <form action="{% url 'chainlettersstories:get_random_available_section' userid %}">
+            <button type="submit">Join</button>
+        </form>
+    </div>
+
+    <div class="tabs">
+        {% for story_dict in story_dicts %}
+        <button class="story-tab-button"
+            id="story-tab-button-{{forloop.counter}}">{{ forloop.counter }}</button>
+        {% endfor %}
+    </div>
+
+    {% for sectionid, story_dict in story_dicts.items %}
+    <div class="content-submissions-comments"
+        id="content-submissions-comments-{{forloop.counter}}">
+        <form class="content-submissions"
+            id="content-submissions-{{forloop.counter}}"
+            action="{% url 'chainlettersstories:submit_section_to_story' userid sectionid %}" method="post">
+            {% csrf_token %}
+            <input class="read-only-content"
+                id="read-only-content-{{forloop.counter}}"
+                type="text"
+                value="{{story_dict.previous}}"
+                readonly>
+                <div id="react-read-only-content"></div>
+                {% vite_asset 'assets/write_dashboard_components.jsx' %}
+                <input class="input-content"
+                    id="input-content-{{forloop.counter}}"
+                    type="text"
+                    placeholder="Enter Story Here"
+                    name="content"
+                    value="{{story_dict.current}}">
+                    <div class="submissions">
+                        <button type="submit" name="save-or-submit" value="save">Save</button>
+                        <button type="submit" name="save-or-submit" value="submit">Submit</button>
+                        <button type="submit" name="save-or-submit" value="abandon">Abandon</button>
+                    </div>
+                </form>
+                <div class="comments">Hello</div>
+            </div>
+            {% endfor %}
+    </div>
+</div>
+</body >
