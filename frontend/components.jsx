0d21c2be055df70b68c47a37799cf7bf20c995c3
jburnstu <75335189+jburnstu@users.noreply.github.com>
2026-02-20 15:26:09 +0000

    It works! (Note that we haven't at all planned aroudn failures, ie no available sections or things like that)

--- frontend/components.jsx
+++ frontend/components.jsx
@@ -152,6 +152,95 @@ function NewButton(props) {
     )
 }
 
+
+function JoinButton(props) {
+
+    console.log(props.userid);
+
+    async function handleSubmit(e) {
+        let availabilityResponse = await fetch(`http://127.0.0.1:8000/api/usersincludingavailability/${props.userid}`,
+            {
+                method: 'get'
+            }
+        )
+
+        if (availabilityResponse.status != 200) {
+            console.log("HTTP Error:", availabilityResponse.status);
+            return;
+        }
+
+        let availabilityData = await availabilityResponse.json();
+        console.log("STORY SUCCESS");
+        console.log(availabilityData);
+
+        let availableSections = availabilityData.availablesections;
+        console.log(availableSections);
+
+        let randomSectionID = availableSections[Math.floor(Math.random() * availableSections.length)]
+        console.log(randomSectionID);
+        console.log(`http://127.0.0.1:8000/api/sections/${randomSectionID}`);
+
+        let updatePreviousSectionResponse = await fetch(`http://127.0.0.1:8000/api/sections/${randomSectionID}/`,
+            {
+                'method': 'patch',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                'body': JSON.stringify({
+                    'sectionstatusid': 5
+                })
+            }
+        );
+
+        if (!updatePreviousSectionResponse.ok) {
+            console.log("HTTP Error:", updatePreviousSectionResponse.status);
+            return;
+        }
+        let updatePreviousSectionData = await updatePreviousSectionResponse.json();
+        console.log("UPDATE PREVIOUS SECTION SUCCESS");
+        console.log(updatePreviousSectionData);
+
+        let sectionCreationResponse = await fetch('http://127.0.0.1:8000/api/sections/',
+            {
+                'method': 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                'body': JSON.stringify({
+                    'storyid': updatePreviousSectionData.storyid,
+                    'userid': props.userid,
+                    'sectionstatusid': 1,
+                    'previoussectionid': randomSectionID
+                })
+            }
+        );
+
+        if (!sectionCreationResponse.ok) {
+            console.log("HTTP Error:", sectionCreationResponse.status);
+            return;
+        }
+
+        let sectionResponseData = await sectionCreationResponse.json();
+
+        console.log("SECTION SUCCESS");
+        console.log(sectionResponseData);
+
+    }
+
+
+
+    return (
+        <button onClick={handleSubmit}>JOIN</button>
+    )
+}
+
+
+
+
+
+
 function Sidebar(props) {
 
     switch (props.sidebarType) {
@@ -159,7 +248,7 @@ function Sidebar(props) {
             return (
                 <div className="sidebar">
                     <NewButton userid={props.userid} />
-                    <GetButton userid={props.userid} link='chainlettersstories:get_random_available_section' name="JOIN" />
+                    <JoinButton userid={props.userid} />
                 </div>
             )
         default:
